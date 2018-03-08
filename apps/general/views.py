e79cb20124e5cf2b200b664e91f5a0cabbbd3c56
ankit1697 <agrawala.1697@gmail.com>
2018-03-08 23:07:10 +0530

    email changed

--- apps/general/views.py
+++ apps/general/views.py
@@ -93,7 +93,7 @@ class HomeView(FormView):
 					'Feedback OTP',
 					'Hi,' + qs.first_name + '\n\n' +'Your OTP for feedback is:' + random_otp + '\n\nThanks,\nBFS-BMSIT' ,
 					'Feedback Support <feedback@bmsit.in>',
-					['agrawala.1697@gmail.com',],
+					['bhargavsagiraju@gmail.com',],
 					)
 		email.send()
 		print('OR: ' + qs.password)
