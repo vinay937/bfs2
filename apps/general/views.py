b241e18a24c9561fe106e438a74d933c87bffa9e
Aayush <nandkeolyar.aayush@gmail.com>
2018-04-06 10:09:50 +0530

    Add percentage

--- apps/general/views.py
+++ apps/general/views.py
@@ -647,13 +647,13 @@ def Dashboard(request):
 	user_type = user.get_user_type()
 	print(user_type[0].name)
 	context = { "user_type": user_type[0].name,
-				"cse" : User.objects.filter(done=False, user_type__name='Student', department__name='CSE').count(),
-				"ece" : User.objects.filter(done=False, user_type__name='Student', department__name='ECE').count(),
-				"mech" : User.objects.filter(done=False, user_type__name='Student', department__name='MECH').count(),
-				"tce" : User.objects.filter(done=False, user_type__name='Student', department__name='TCE').count(),
-				"eee" : User.objects.filter(done=False, user_type__name='Student', department__name='EEE').count(),
-				"civil" : User.objects.filter(done=False, user_type__name='Student', department__name='CIVIL').count(),
-				"ise" : User.objects.filter(done=False, user_type__name='Student', department__name='ISE').count(),
+				"cse" : ((User.objects.filter(done=False, user_type__name='Student', department__name='CSE').count())/(User.objects.filter(user_type__name='Student', department__name='CSE').count()))*100,
+				"ece" : ((User.objects.filter(done=False, user_type__name='Student', department__name='ECE').count())/(User.objects.filter(user_type__name='Student', department__name='ECE').count()))*100,
+				"mech" : ((User.objects.filter(done=False, user_type__name='Student', department__name='MECH').count())/(User.objects.filter(user_type__name='Student', department__name='MECH').count()))*100,
+				"tce" : ((User.objects.filter(done=False, user_type__name='Student', department__name='TCE').count())/(User.objects.filter(user_type__name='Student', department__name='TCE').count()))*100,
+				"eee" : ((User.objects.filter(done=False, user_type__name='Student', department__name='EEE').count())/(User.objects.filter(user_type__name='Student', department__name='EEE').count()))*100,
+				"civil" : ((User.objects.filter(done=False, user_type__name='Student', department__name='CIV').count())/(User.objects.filter(user_type__name='Student', department__name='CIV').count()))*100,
+				"ise" : ((User.objects.filter(done=False, user_type__name='Student', department__name='ISE').count())/(User.objects.filter(user_type__name='Student', department__name='ISE').count()))*100,
 
 	}
 	return render(request, template_name, context)
