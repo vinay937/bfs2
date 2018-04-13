94d70c5c0ae8b7d3cd6449b34c3740452db4d06b
Aayush <nandkeolyar.aayush@gmail.com>
2018-04-13 11:09:54 +0530

    gTvazE/Ohmko7OqA

--- apps/feedback/urls.py
+++ apps/feedback/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^report/', views.Report.as_view(), name='report'),
     url(r'^__/__/--/__/__reports/(?P<username>[-\w]+)/$',views.consolidated, name='reports-url'),
     url(r'^reportstudent/(?P<username>[-\w]+)/$',views.Student_Report.as_view(), name='reportstudent'),
-    url(r'^sreports/(?P<username>[-\w]+)/$',views.sconsolidated, name='sconsolidated'),
+    url(r'^sreports/(?P<username>[-\w]+\.)/$',views.sconsolidated, name='sconsolidated'),
     url(r'^report-select-hod/$',views.select_teacher_hod.as_view(), name='hod-select-faculty'),
 ]
