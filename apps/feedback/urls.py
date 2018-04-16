a16ce8e816195480da11c545abc1e37cc0d8519e
amoghng7 <amoghsk279@gmail.com>
2018-04-16 16:58:08 +0530

    63C7KwfCAIKFHsB9

--- apps/feedback/urls.py
+++ apps/feedback/urls.py
@@ -5,12 +5,12 @@ from . import views
 urlpatterns = [
     url(r'^entry/', views.FeedbackView.as_view(), name='feedback_form'),
     url(r'^report/', views.Report.as_view(), name='report'),
-    url(r'^__/__/--/__/__reports/(?P<username>[-\w]+)/$',views.consolidated, name='reports-url'),
+    # url(r'^__/__/--/__/__reports/(?P<username>[-\w]+)/$',views.consolidated, name='reports-url'),
     url(r'^reportstudent/(?P<username>[-\w]+)/$',views.Student_Report.as_view(), name='reportstudent'),
     url(r'^sreports/(?P<username>[-\w.]+)/$',views.sconsolidated, name='sconsolidated'),
     url(r'^report-select-hod/$',views.select_teacher_hod.as_view(), name='hod-select-faculty'),
     url(r'^consolidated/$',views.student_view_consolidated, name='consolidated'),
     url(r'^consolidated_sixty/$',views.student_view_consolidated_sixty, name='consolidated_sixty'),
     url(r'^report-principal/$',views.select_teacher_principal.as_view(), name='principal-select-faculty'),
-    url(r'^__/__/--/__/__sreports/(?P<username>[-\w.]+)/$',views.Test_report, name='sreports-url'),
+    # url(r'^__/__/--/__/__sreports/(?P<username>[-\w.]+)/$',views.Test_report, name='sreports-url'),
 ]
